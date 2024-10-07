a35b60369029d63912c2851a20672db54797c59b
Penn Wang <guyusay@gmail.com>
2024-10-07 11:26:34 +0800

    fix: wrapper useSearchParams in suspense

--- src/app/print/page.tsx
+++ src/app/print/page.tsx
@@ -1,16 +1,27 @@
 'use client'
 import Row from '@/components/Row'
 import { useSearchParams } from 'next/navigation'
-export default function PrintPage() {
+import { Suspense } from 'react'
+
+function Page() {
   const searchParams = useSearchParams()
   const text = searchParams.get('text') ?? '\n\n\n\n'
   const theme = searchParams.get('theme') ?? 'default'
   const words = text.split(/\s/)
+
   return (
     <div className={`p-4 flex flex-col gap-2 text-2xl zt-theme-${theme}`}>
       {words.map((word, index) => (
         <Row key={index} word={word} />
       ))}
     </div>
+  )
+}
+
+export default function PrintPage() {
+  return (
+    <Suspense>
+      <Page />
+    </Suspense>
   )
 }
